7eb2e30c1f0b9d0ee430cbdb49d6b8f361546895
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2022-07-15 16:05:16 +0300

    Week 1

---
+++ foreignLanguages.js
@@ -0,0 +1,10 @@
+function languages(input){
+    let country = input;
+    if(country ==="England" || country ==="USA"){
+        console.log("English");
+    } else if(country ==="Spain"||country ==="Argentina"|| country==="Mexico"){
+        console.log("Spanish");
+    } else {
+        console.log("unknown");
+    }
+}languages("GeUSA")
