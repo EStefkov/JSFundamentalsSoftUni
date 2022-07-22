5c1223d0a5d91acb8bf1761700e5a6b1bd2d781e
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2022-07-22 16:42:07 +0300

    Create firstLast.js

---
+++ arrays/firstLast.js
@@ -0,0 +1,6 @@
+function firstLast(numbers){
+    let firstEl=numbers[0];
+    let lastEl=numbers[numbers.length -1];
+    let sum = firstEl+lastEl;
+    console.log(sum);
+}firstLast([4,2,3,4,5,6,9])
