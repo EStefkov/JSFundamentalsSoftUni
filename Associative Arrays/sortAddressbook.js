1063cc6054d0ebda5485ae5c9e3aabbe40680ec0
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2023-07-28 15:24:07 +0300

    Update sortAddressbook.js

--- Associative Arrays/sortAddressbook.js
+++ Associative Arrays/sortAddressbook.js
@@ -4,16 +4,20 @@ function sortAdressbook(input){
         let [name, address] = line.split(':');
         catalog[name] = address;
     }
+    //vzimame catalog kolekciqta  i izvlichame spisuk na kluchovete i stoinostite kato massiv
    let sorted = Object.entries(catalog);
+   //sortirame kato polzvame sort i znaem che a, i b  sa masivi s  kluch i stoinost 
     sorted.sort((a,b) => {
+        //zapazvame gi kato promenlivi
         let nameA =a[0];
         let nameB =b[0];
+        // vrushtame kato gi sravnqvame 
         return nameA.localeCompare(nameB);
     })
     for (let name in catalog){
         console.log(`${name} -> ${catalog[name]}`);
     }
-    
+
     for(let [name,address] of sorted){
         console.log(`${name} ------> ${address}`);
     }
