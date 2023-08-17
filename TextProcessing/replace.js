044356b7ff524329bee0178ce643f5b1fd87455a
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2023-08-17 18:32:06 +0300

    Update replace.js

--- TextProcessing/replace.js
+++ TextProcessing/replace.js
@@ -1,3 +1,15 @@
 function solve(text,word){
-     sadasdas 
+     let start = text.indexOf(word);
+
+     while (start!=-1){
+        let first  = text.substring(0,start);
+        let second = text.substring(start + word.length);
+        text = first + '*'.repeat(word.length) + second;
+
+        start = text.indexOf(word);
+        
+     }
+     console.log(text);
 }
+
+solve('A small sentence small with some words','small');
